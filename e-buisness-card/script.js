efdcf95c1bb9dddb0dd7449c07d700d0eafaa6d8
Aakash Tandew <aakash.tandew1996@gmail.com>
2023-09-27 16:46:55 +0530

    add to contact logic added

--- e-buisness-card/script.js
+++ e-buisness-card/script.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("add-contact").addEventListener("click", function () {
-    console.log("clicked");
-    if ("contacts" in navigator) {
-      const contact = new Contact({
-        name: "John Doe",
-        email: "johndoe@example.com",
-        phoneNumbers: [{ value: "123-456-7890", type: "mobile" }],
-      });
+  var saveBtn = document.getElementById("add-contact");
+  saveBtn.addEventListener("click", function () {
+    // Get the contact information from the website
+    var contact = {
+      name: "Shreyansh Mogari",
+      phone: "+917999650832",
+      email: "shreyansh@pandaadvertising.in",
+    };
+    // create a vcard file
+    var vcard =
+      "BEGIN:VCARD\nVERSION:4.0\nFN:" +
+      contact.name +
+      "\nTEL;TYPE=work,voice:" +
+      contact.phone +
+      "\nEMAIL:" +
+      contact.email +
+      "\nEND:VCARD";
+    var blob = new Blob([vcard], { type: "text/vcard" });
+    var url = URL.createObjectURL(blob);
 
-      const options = { contact };
-      navigator.contacts
-        .save(options)
-        .then(() => {
-          alert("Contact saved successfully");
-        })
-        .catch((error) => {
-          alert("Error saving contact: " + error);
-        });
-    } else {
-      alert("The Contacts API is not supported on this device.");
-    }
+    const newLink = document.createElement("a");
+    newLink.download = contact.name + ".vcf";
+    newLink.textContent = contact.name;
+    newLink.href = url;
+
+    newLink.click();
   });
 });
